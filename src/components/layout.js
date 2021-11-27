fab8cec287e34dfbc6684a123d8d33b2e8b030c0
Jan W. Hiermayer | suxxess solution GmbH <jwh@suxxess.tech>
2021-11-27 10:12:43 +0100

    Finished Gatsby Tutorial Part 4

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,25 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-import { container,heading,navLinks,navLinkItem,navLinkText } from './layout.module.css'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import { container,heading,navLinks,navLinkItem,navLinkText,siteTitle } from './layout.module.css'
 
 
-const layout =({ pageTitle, children}) => {
+
+
+const Layout =({ pageTitle, children}) => {
+    const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
     return (
         <div className={container}>
-            <title>{pageTitle}</title>
+            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+            <header className={siteTitle}>{data.site.siteMetadata.title}</header> 
             <nav>
                 <ul className={navLinks}>
                     <li className={navLinkItem}>
@@ -14,6 +27,9 @@ const layout =({ pageTitle, children}) => {
                     </li>
                     <li className={navLinkItem}> 
                         <Link to="/about" className={navLinkText}>About</Link>
+                    </li>
+                    <li className={navLinkItem}> 
+                        <Link to="/blog" className={navLinkText}>Blog</Link>
                     </li>
                 </ul>
             </nav>
@@ -25,4 +41,4 @@ const layout =({ pageTitle, children}) => {
     )
 }
 
-export default layout
+export default Layout
